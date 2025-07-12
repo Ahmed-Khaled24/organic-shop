0f8891362de3af74ee9752a78ea1c36f23a25780
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-07-12 14:55:07 +0300

    feat: create new component product card

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,52 @@
+import { type FC } from "react";
+import type { Product } from "../utils/db";
+import { Link } from "react-router";
+
+const ProductCard: FC<Product> = (product) => {
+    let price = product.price;
+    if (product.hasDiscount) {
+        price = Math.trunc(
+            product.price - product.price * (product.discountPercentage! / 100),
+        );
+    }
+    const formattedOriginalPrice = new Intl.NumberFormat("en-En", {
+        style: "currency",
+        currency: "USD",
+    }).format(product.price);
+    const formattedCurrentPrice = new Intl.NumberFormat("en-En", {
+        style: "currency",
+        currency: "USD",
+    }).format(price);
+
+    return (
+        <Link to={`products/${product.id}`} className="relative">
+            <img src={product.mainImage} alt="" />
+
+            {/* Text info */}
+            <div className="flex flex-col items-center py-4">
+                <p className="opacity-50 text-sm">{product.category}</p>
+                <h6 className="font-merriweather!">{product.title}</h6>
+                <div className="before:content-['☆☆☆☆☆']"></div>
+                <div className="flex gap-2">
+                    {product.hasDiscount && (
+                        <del className="font-semibold text-sm opacity-50">
+                            {formattedOriginalPrice}
+                        </del>
+                    )}
+                    <p className="font-semibold text-sm">
+                        {formattedCurrentPrice}
+                    </p>
+                </div>
+            </div>
+
+            {/* Sale badge */}
+            {product.hasDiscount && (
+                <div className="bg-green-primary font-semibold text-white text-xs rounded-full absolute -top-2 -right-2 h-10 w-10 flex items-center justify-center">
+                    <span>Sale!</span>
+                </div>
+            )}
+        </Link>
+    );
+};
+
+export default ProductCard;
