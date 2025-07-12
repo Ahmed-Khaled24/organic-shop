42c926c040a6eaaea8464d44c584229dc22df000
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-07-12 14:32:50 +0300

    feat: update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,31 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home";
+import { PublicLayout } from "../layouts/PublicLayout";
+import { Contact } from "../pages/Contact";
+import { About } from "../pages/About";
+import { Products } from "../pages/Products";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        Component: Home,
+        Component: PublicLayout,
+        children: [
+            {
+                index: true,
+                Component: Home,
+            },
+            {
+                path: "contact",
+                Component: Contact,
+            },
+            {
+                path: "about",
+                Component: About,
+            },
+            {
+                path: "products",
+                Component: Products,
+            },
+        ],
     },
 ]);
