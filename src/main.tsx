acd9d89e9235155891b3118f6622b0f8f84560de
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-07-26 00:46:37 +0300

    feat: move router provider into app

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { RouterProvider } from "react-router";
-import { router } from "./routes/index.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <RouterProvider router={router} />
         <Provider store={store}>
             <App />
         </Provider>
